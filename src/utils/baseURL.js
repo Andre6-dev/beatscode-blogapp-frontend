f706c892246a741d93f6ef5e403182106e11578b
Andre Gallegos <andre.gallegos@tecsup.edu.pe>
2022-07-07 09:32:26 -0500

    Adding railway port

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,4 +1,5 @@
 const baseUrlLocal = "http://localhost:5000";
+const urlRailway = "https://beatscodeblogappbackend-production.up.railway.app";
 
 const baseUrl = process.env.REACT_APP_API_URL;
-export default baseUrlLocal;
+export default urlRailway;
