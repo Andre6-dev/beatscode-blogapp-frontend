111ae822bc37efa639b548c9866a63f46af226ab
Andre Gallegos <andre.gallegos@tecsup.edu.pe>
2022-07-07 11:41:28 -0500

    Final deployment with link fixed

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -2,4 +2,4 @@ const baseUrlLocal = "http://localhost:5000";
 const urlRailway = "https://beatscodeblogappbackend-production.up.railway.app";
 
 const baseUrl = process.env.REACT_APP_API_URL;
-export default baseUrl;
+export default urlRailway;
